bde1ec3a702b569ca37e476d71468b8cae42f060
Cesar Suriano <cesaraugustokg6@hotmail.com>
2018-12-26 22:24:52 -0200

    componente personalização

--- front/src/components/sabor/Sabor.js
+++ front/src/components/sabor/Sabor.js
@@ -4,7 +4,19 @@ import './Sabor.css';
 class Sabor extends Component {
   render() {
     return (
-     <div>Ola Sabor</div>
+      <div>
+      <header>
+        <a href="" class="logo">Logo</a>
+        <button>Download</button>
+      </header>
+      <div class="conteudo">
+        <h2>Selecione o sabor de sua pizza</h2>
+        <a><div class="card">Calabresa</div></a>
+        <a><div class="card">Marquerita</div></a>
+        <a><div class="card">Portuguesa</div></a>
+        <button>Finalizar meu pedido</button>
+      </div>
+    </div>
     );
   }
 }
